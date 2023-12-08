451d8993f07261bd2485616caf4bf91720999e5d
Febrie Zaeni Muchtar <febriezaenimuchtar@gmail.com>
2023-12-08 13:26:21 +0700

    first commit

---
+++ bukutamu.php
@@ -0,0 +1,82 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#">Kesimpulan </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="bukutamu.php">Buku tamu</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+<div class="container">
+    <h1>Buku Tamu</h1>
+
+    <form action ="" method="POST" name ="penjualan">
+        
+  <div class="form-group">
+    <label for="nm">Nama </label>
+    <input type="text" class="form-control" id="nm" name="nm" Required>
+  </div>
+  <div class="form-group">
+    <label for="kelas">kelas</label>
+    <input type="text" class="form-control" id="kelas" name="kelas" Required>
+  </div>
+  <div class="form-group">
+    <label for="jurusan">jurusan</label>
+    <input type="text" class="form-control" id="jurusan" name="jurusan" Required>
+  </div>
+  
+  <button type="submit" name="submit" class="btn btn-primary">Sumbit</button>
+  
+</form>
+
+<hr>
+    <?php
+    if(isset($_POST['submit'])){
+      $nama =$_POST['nm'];
+      $kelas =$_POST['kelas'];
+      $jurusan =$_POST['jurusan'];
+
+    
+    
+    ?>
+     <div class="container">
+
+            <table class='table table-bordered'>
+                <thead>
+                    <tr>
+                        <th scope='col'>No</th>
+                        <th scope='col'>Nama</th>
+                        <th scope='col'>Kelas</th>
+                        <th scope='col'>Jurusan</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope='row'>1</th>
+                        <td><?php echo $nama ?></td>
+                        <td><?php echo $kelas ?></td>
+                        <td><?php echo $jurusan ?></td>
+                        
+                    </tr>
+
+                </tbody>
+            </table>
+
+    </div>
+    <?php } ?>
+    </div>
---
+++ home.php
@@ -0,0 +1,86 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <title>HARI KESAKTIAN PANCASILA</title>
+  </head>
+  <body>
+      
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="kesimpulan.php">Kesimpulan </a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="bukutamu.php">Buku tamu</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+<p><!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+
+    <title>HARI KESAKTIAN PANCASILA</title>
+  </head>
+  <body>
+  <div class="container">
+    <div class="card mb-3">
+        <img src="img/download.jpg" class="card-img-top" alt="..." height="270px ">
+
+  <div class="card" style="width: 18rem;">
+    <div class="container">
+       <img src="img/poto.png" alt="" width="200px">
+            
+<p>Hari Kesaktian Pancasila adalah hari nasional di Indonesia yang diperingati setiap tanggal 1 Oktober sesuai dengan Keputusan Presiden Nomor 153 Tahun 1967. </p>
+<p>5 tahun yang lalu, pecah Gerakan 30 September (G30S) yang menewaskan beberapa perwira Angkatan Darat--mereka lantas ditetapkan sebagai pahlawan revolusi. Apa yang terjadi setelahnya sudah kita ketahui bersama: pembantaian massal terhadap orang-orang yang dituding PKI atau komunis. Dikutip dari Menguak Misteri Sejarah (2010) karya Asvi Warman Adam</p>
+           
+<p>Penanaman Pancasila sudah diinisiasi sejak lama, melalui pendidikan di sekolah, sosialisasi hingga lirik lagu nasional. Sehubungan dengan ini, pembahasan Pancasila perlu dilakukan secara menyeluruh. </p>
+
+        
+    </div>
+ 
+    </div>
+</div>    </div>
+</div>
+</div>
+   
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
+    -->
+  
+ 
